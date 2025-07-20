dd44d340192d4ba60f558ae0fdcea656fb7bc0c3
sanjaykumar-rgb <sanjayk85100@gmail.com>
2025-07-20 16:55:01 +0530

    3rd commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // 👈 required for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
