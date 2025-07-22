16967fa1dbd74cea5de9d9c48529f5c8a8566582
sanjaykumar-rgb <sanjayk85100@gmail.com>
2025-07-22 22:14:56 +0530

    2nd commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Portfolio/', // 👈 required for GitHub Pages
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer({ open: true })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL('./src', import.meta.url)),
